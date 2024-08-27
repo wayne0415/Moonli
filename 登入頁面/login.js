11c09ba1e60eeb978dd39aef5d02677a7c3778d4
wayne0415 <wayne20003106@gmail.com>
2024-08-27 16:36:34 +0800

    asdfasdf

--- 登入頁面/login.js
+++ 登入頁面/login.js
@@ -1,24 +1,23 @@
-const tabs = document.querySelectorAll(".tabs button");
-const loginButton = document.querySelector(".login-button");
+const tab_btn = document.querySelectorAll(".tab-btn");
 
-tabs.forEach((tab) => {
+tab_btn.forEach((tab) => {
     tab.addEventListener("click", () => {
-        tabs.forEach((t) => t.classList.remove("active"));
+        tab_btn.forEach((t) => t.classList.remove("active"));
         tab.classList.add("active");
         // 這裡可以加入切換不同表單的邏輯
     });
 });
 // 按註冊改login-button的文字變成下一步
-const loginButton_changeName = document.getElementById("login-button");
-const reg_btn = document.getElementById("reg-btn");
-const loginButton1 = document.getElementById("login-button1");
-reg_btn.addEventListener("click", () => {
+const tab_btn_login = document.getElementById("tab-btn-login");
+const tab_btn_logout = document.getElementById("tab-btn-logout");
+const loginButton = document.getElementById("loginButton");
+tab_btn_logout.addEventListener("click", () => {
     if (loginButton.textContent === "登入") {
         loginButton.textContent = "下一步";
     }
 });
 // 按登入改login-button的文字變成登入
-loginButton1.addEventListener("click", () => {
+tab_btn_login.addEventListener("click", () => {
     if (loginButton.textContent === "下一步") {
         loginButton.textContent = "登入";
     }
