de03ce8fe17950bcad92cf656670622bf949861d
wayne0415 <wayne20003106@gmail.com>
2024-08-30 04:28:27 +0800

    123

--- javascripts/landing.js
+++ javascripts/landing.js
@@ -2,7 +2,7 @@ let stupid_6q = document.getElementById("stupid-6q");
 let problems_title = document.getElementById("problems-title");
 let fucking_leaf = document.getElementById("fucking-leaf");
 let fucking_mountain = document.getElementById("fucking-mountain");
-// let text_very = document.getElementById("text_very");
+let small_mountain = document.getElementById("small-mountain");
 // let text_iphone = document.getElementById("text_iphone");
 // let text_iphone_plus = document.getElementById("text_iphone_plus");
 // let iphone_14 = document.getElementById("iphone_14");
@@ -29,6 +29,7 @@ window.addEventListener("scroll", () => {
     if (a > 1950) {
         let offset = a - 1950;
         fucking_leaf.style.bottom = `${offset * 0.1}px`;
+        small_mountain.style.bottom = `${offset * 0.1}px`;
     }
     if (a > 1700) {
         let offset = a - 1700;
