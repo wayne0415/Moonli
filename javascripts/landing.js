104e2c0cea0cc45bb420fc41f1e9d59f49ae364a
wayne0415 <wayne20003106@gmail.com>
2024-08-30 04:13:40 +0800

    1223

--- javascripts/landing.js
+++ javascripts/landing.js
@@ -1,28 +1,40 @@
-let bg = document.getElementById("bg");
-let logo = document.getElementById("logo");
-let btn = document.getElementById("btn");
-let text_big = document.getElementById("text_big");
-let text_very = document.getElementById("text_very");
-let text_iphone = document.getElementById("text_iphone");
-let text_iphone_plus = document.getElementById("text_iphone_plus");
-let iphone_14 = document.getElementById("iphone_14");
-let iphone_14_plus = document.getElementById("iphone_14_plus");
+let stupid_6q = document.getElementById("stupid-6q");
+let problems_title = document.getElementById("problems-title");
+let fucking_leaf = document.getElementById("fucking-leaf");
+let fucking_mountain = document.getElementById("fucking-mountain");
+// let text_very = document.getElementById("text_very");
+// let text_iphone = document.getElementById("text_iphone");
+// let text_iphone_plus = document.getElementById("text_iphone_plus");
+// let iphone_14 = document.getElementById("iphone_14");
+// let iphone_14_plus = document.getElementById("iphone_14_plus");
+
+// 函數來處理顯示與隱藏
+function toggleVisibility(x, y, z, n, element, triggerPoint, scrollPosition) {
+    if (scrollPosition > triggerPoint) {
+        element.style.opacity = x;
+        element.style.transform = `scale(${y})`;
+    } else if (scrollPosition <= triggerPoint) {
+        element.style.opacity = z;
+        element.style.transform = `scale(${n})`;
+    }
+}
 
 window.addEventListener("scroll", () => {
+    // let b = window.innerHeight;
     let a = window.scrollY;
-    let b = window.innerHeight;
+    console.log(a);
+    // 呼叫函數來處理 problems_title 和 stupid_6q
+    toggleVisibility(1, 1, 0, 0, problems_title, 1500, a);
+    toggleVisibility(1, 0.9, 0, 0, stupid_6q, 1600, a);
+    if (a > 1950) {
+        let offset = a - 1950;
+        fucking_leaf.style.bottom = `${offset * 0.1}px`;
+    }
+    if (a > 1700) {
+        let offset = a - 1700;
+        fucking_mountain.style.bottom = `${offset * -0.08}px`;
+    }
 
-    bg.style.left = a * 0.15 + "px";
-    logo.style.marginBottom = a * 1 + "px";
-    btn.style.marginBottom = a * 0.5 + "px";
-    text_big.style.top = a * -0.3 + "px";
-    text_very.style.top = a * -0.37 + "px";
-    text_iphone.style.marginLeft = a * -0.4 + "px";
-    text_iphone_plus.style.marginRight = a * -0.45 + "px";
-
-    iphone_14.style.marginLeft = a * -0.13 + "px";
-    iphone_14_plus.style.marginRight = a * -0.15 + "px";
-
-    iphone_14.style.top = -0.1 * b + a * 0.13 + "px";
-    iphone_14_plus.style.top = -0.1 * b + a * 0.15 + "px";
+    // btn.style.marginBottom = a * 0.5 + "px";
+    // text_big.style.top = a * -0.3 + "px";
 });
